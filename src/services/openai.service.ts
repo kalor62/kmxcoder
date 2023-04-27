6bc881c6dbb30ffeda532f1d138241e2ee4ec515
kalor62 <karol.mroszczyk@gmail.com>
2023-04-27 16:29:13 +0200

    - improvements to openai service made by kmxcoder itself

--- src/services/openai.service.ts
+++ src/services/openai.service.ts
@@ -9,12 +9,9 @@ export default class OpenaiService {
    constructor() {
       const config = vscode.workspace.getConfiguration(this.extensionName);
       const apiKey = config.get<string>('openaiApiKey');
-      const openaiEngineModel = config.get<string>('openaiEngineModel');
-      this.openaiEngineModel = typeof (openaiEngineModel) === 'string' ? openaiEngineModel : '';
+      this.openaiEngineModel = config.get<string>('openaiEngineModel') || '';
 
-      const configuration = new OpenAI.Configuration({
-         apiKey: apiKey
-      });
+      const configuration = new OpenAI.Configuration({ apiKey: apiKey });
 
       this.openai = new OpenAI.OpenAIApi(configuration);
    }
@@ -26,11 +23,11 @@ export default class OpenaiService {
             messages: [{ role: "user", content: text }]
          });
 
-         const suggestions = response?.data?.choices[0]?.message?.content;
+         const suggestions = response?.data?.choices?.[0]?.message?.content;
 
          return suggestions;
       } catch (error: any) {
-         vscode.window.showErrorMessage('Error: ' + error.message);
+         vscode.window.showErrorMessage(`Error: ${error.message}`);
       }
    };
 }
